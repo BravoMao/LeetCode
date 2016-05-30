4af797adab1c734ad9df42ef676663842d434d48
jianli zhang <jianli.zhang@havasmedia.com>
2016-05-30 17:55:05 +0200

    leetcode_334 update

--- LeetCode_334.py
+++ LeetCode_334.py
@@ -1,10 +1,23 @@
 class Solution(object):
     def increasingTriplet(self, nums):
-        dict={}
+        min,mid,max=None,None,None
+        for index in range(0,len(nums))[::-1]:
+            if(nums[index]>max):
+                max=nums[index]
+                mid=None
+                continue
+            if(nums[index]==max):
+                continue
+            if(nums[index]>=mid):
+                mid=nums[index]
+                continue
+            if(nums[index]>min):
+                return True
+
+        return False
+
+
 if __name__ == '__main__':
-
-    for index in range(0, 5)[::-1]:
-        print index
-
-
-    #test master
+    nums=[1,1,-2,6]
+    solution=Solution()
+    print(solution.increasingTriplet(nums))
